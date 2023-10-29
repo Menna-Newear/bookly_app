2cf5df127477d33dfdbc9d66f56b4b6210965062
Menna-Newear <mennanewear@gmail.com>
2023-10-29 06:12:56 +0200

    Adding Title And Author to Book Details

--- lib/features/widgets/bookDetailsViewBody.dart
+++ lib/features/widgets/bookDetailsViewBody.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utilits/styles.dart';
 import 'package:bookly_app/features/widgets/customBookDetailsAppBar.dart';
 import 'package:bookly_app/features/widgets/customBookImage.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +16,25 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
+          ),
+          Text(
+            "The Jungle Book ",
+            style: Styles.teststyle30.copyWith(
+              fontFamily: kGTSectraFine,
+            ),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text("Rudyard Kipling ",
+                style: Styles.titleMedium18.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                )),
           ),
         ],
       ),
