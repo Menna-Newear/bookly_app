76d645ecde1dd1c39279d027aa39488a32b81147
Menna-Newear <mennanewear@gmail.com>
2023-11-12 03:46:13 +0200

    create FatiredBooks Method

--- lib/features/Home/persentation/manger/featuredBooksCubit/featured_books_cubit.dart
+++ lib/features/Home/persentation/manger/featuredBooksCubit/featured_books_cubit.dart
@@ -1,9 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/Home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> FetchFeatcherdBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) => emit(FeaturedBooksFailure(failure.errorMessage)),
+        (books) => emit(FeaturedBooksSuccess(books)));
+  }
 }
