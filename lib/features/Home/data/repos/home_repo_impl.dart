3ec7c3087cb80ab36f5e52473a6313a81b2113f9
Menna-Newear <mennanewear@gmail.com>
2023-11-13 04:29:30 +0200

    implement fetch Relevance Books

--- lib/features/Home/data/repos/home_repo_impl.dart
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -45,4 +45,23 @@ class HomeRepoImpl implements HomeRepo {
       return left(ServerFailure(e.toString()));
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchReleventBook() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&q=subject:computer science &Sorting=relevance");
+      List<BookModel> booksList = [];
+      for (var item in data["items"]) {
+        booksList.add(BookModel.fromJson(item));
+      }
+      return right(booksList);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
